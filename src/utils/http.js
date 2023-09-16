381f9c0540b4f57be05f918c3fbda7ce752879bc
Shubham Patrick <ahthneeuhl@gmail.com>
2023-09-16 07:36:06 +0530

    Dynamic Query Functions & Query Keys

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,5 +1,9 @@
-export async function fetchEvents() {
-  const response = await fetch("http://localhost:3000/events");
+export async function fetchEvents(searchQuery) {
+  const url = searchQuery
+    ? `http://localhost:3000/events?search=${searchQuery}`
+    : "http://localhost:3000/events";
+
+  const response = await fetch(url);
 
   if (!response.ok) {
     const error = new Error("An error occurred while fetching the events");
